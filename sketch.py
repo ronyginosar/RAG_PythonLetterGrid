0df3d64e1423c1cb8607e717eabfcf536880ee93
rony ginosar <rony.ginosar@mail.huji.ac.il>
2020-11-22 15:42:25 +0200

    try to fill line

--- sketch.py
+++ sketch.py
@@ -3,39 +3,46 @@
 from drawBot import * # needed for using as python module
 
 # CONSTANTS:
-DISPLAYWIDTH, DISPLAYHEIGHT = 500, 500
-EXCLUDECOLUMN = 1 # todo can we do this in the preprocessing?
+DISPLAYTEXT = "מטבחים"
+# DISPLAYTEXT = "מטבחים מערכות מבטחים"
+EXCLUDECOLUMN = 4 # todo can we do this in the preprocessing?
 FONTSIZE = 150
 FONT = 'assets/RAG-Marom-GX.ttf'
 TOTALFRAMES = 1
 TOTALDURATION = 1 # in seconds
 BACKGROUND_R, BACKGROUND_G, BACKGROUND_B = 238, 127, 171
+MARGINS = 10
 
-
-# PRE PROCESSING
+# PRE PROCESSING - TEXT
 displayTextList = DISPLAYTEXT.split(' ')
+for line in displayTextList:
+    print((list(line)))
 lines = len(displayTextList) # The number of lines
 charsInLine = len(displayTextList[0])
-
+# PRE PROCESSING - FONT
 font(FONT)
-fontMinWidth  = listFontVariations()['wdth']['minValue']
-fontMaxWidth  = listFontVariations()['wdth']['maxValue']
+fontMinWidth = listFontVariations()['wdth']['minValue']
+fontMaxWidth = listFontVariations()['wdth']['maxValue']
 fontMinHeight = listFontVariations()['hght']['minValue']
 fontMaxHeight = listFontVariations()['hght']['maxValue']
-
-minDivWidth = DISPLAYWIDTH * 0.1
-maxDivWidth = DISPLAYWIDTH * 0.3
-minDivHeight = DISPLAYHEIGHT/2 * 0.7
-maxDivHeight = DISPLAYHEIGHT/2
+# PRE PROCESSING - DISPLAY
+DISPLAYWIDTH, DISPLAYHEIGHT = 500, FONTSIZE*lines
+SCREENWIDTH = DISPLAYWIDTH + MARGINS*2
+SCREENHEIGHT = DISPLAYHEIGHT + MARGINS*2
+minLetterWidth = int(DISPLAYWIDTH * 0.1)
+maxLetterWidth = int(DISPLAYWIDTH * 0.5)
+minLetterHeight = int(DISPLAYHEIGHT/2 * 0.7)
+maxLetterHeight = int(DISPLAYHEIGHT/2)
 
 frame_Duration = TOTALDURATION / TOTALFRAMES
 
 # INITIALIZE VARIABLES
-xOffset =  5
-yOffset = DISPLAYHEIGHT - FONTSIZE
+xOffset = 0
+yOffset = SCREENHEIGHT - FONTSIZE
+# xOffset = SCREENWIDTH - MARGINS
+# yOffset = SCREENHEIGHT - MARGINS
 accumulatedWidth = 0
-accumulatedheight = 0
-
+accumulatedHeight = [0] * charsInLine # acc height per column
 
 def newLine():
     t = FormattedString()
@@ -45,26 +52,83 @@ def newLine():
     t.openTypeFeatures(ss01=True)
     return t
 
+# print(minLetterWidth, maxLetterWidth, minLetterHeight, maxLetterHeight) # TEST
+def getVariableSettings(row, col): #, currentlinewidth):
+    global accumulatedWidth, accumulatedHeight
+    if (row==0):
+        if (col==EXCLUDECOLUMN):
+            # print("EXCLUDECOLUMN")
+            currentwidth = minLetterWidth
+            variableWdth = fontMinWidth
+        # variableWdth decided regarding letters before
+        currentmaxwidth = max((DISPLAYWIDTH - accumulatedWidth)*0.5, minLetterWidth)
+        currentwidth = randint(minLetterWidth, int(currentmaxwidth))
+        variableWdth = mapWidthToFont(currentwidth)
+        if (col==charsInLine-1):
+            # currentwidth = maxLetterWidth
+            currentwidth = DISPLAYWIDTH - accumulatedWidth
+            # currentwidth = DISPLAYWIDTH - currentlinewidth
+            print (currentwidth)
+            variableWdth = mapWidthToFont(currentwidth)
+
+        accumulatedWidth += currentwidth
+
+
+    # variableHght = randint(fontMinHeight, fontMaxHeight)
+    variableHght = 100
+    # accumulatedHeight[col] += variableHght
+
+    print ("current range", int(currentmaxwidth), minLetterWidth,
+            "currentwidth", currentwidth,
+            "variableWdth", variableWdth,
+            "accumulatedWidth", accumulatedWidth)
+    return variableWdth, variableHght
+
+
+def mapWidthToFont(currentwidth):
+    return int(mapValue(currentwidth,
+                    minLetterWidth, maxLetterWidth,
+                    fontMinWidth, fontMaxWidth))
+
+def mapValue(value, oldmin, oldmax, newmin, newmax):
+    return (value - oldmin) / (oldmax - oldmin) * (newmax - newmin) + newmin
+
 # MAIN
 newDrawing() # needed for using as python module
 for frame in range(TOTALFRAMES):
     # Draw the background rectangle
-    newPage(DISPLAYWIDTH, DISPLAYHEIGHT)
+    newPage(SCREENWIDTH, SCREENHEIGHT)
     fill(BACKGROUND_R/255, BACKGROUND_G/255, BACKGROUND_B/255)
-    rect(0,0,DISPLAYWIDTH, DISPLAYHEIGHT)
+    rect(0,0,SCREENWIDTH, SCREENHEIGHT)
+    # test
+    fill(0,0,0) # test
+    rect(MARGINS, MARGINS ,DISPLAYWIDTH, DISPLAYHEIGHT) # test
+    # test
+    fill(0,0,0) # test
+    rect(SCREENWIDTH-MARGINS, SCREENHEIGHT-MARGINS ,10, 10) # test
+
     frameDuration(frame_Duration)
     for line in range(lines):
         # Create a new empty text object and set its properties
         lineTxt = newLine()
         for char in range(charsInLine):
-            txt.append(
-                displayTextList[line][char]
-                # , # the char
-                # fontVariations = dict(hght = hght_value) # create dict to put inplace of original fontVariations dict
-                # fontVariations(wght=varWght) ??
+            currentlinewidth = int(textSize(lineTxt)[0])
+            print(displayTextList[line][char], currentlinewidth)
+            # lineTxt = newLine()
+            variableWdth, variableHght = getVariableSettings(line, char) #, currentlinewidth)
+            lineTxt.append(
+                displayTextList[line][char],
+                # create dict to replace original fontVariations - so instead of fontVariations(x)
+                fontVariations = dict(wdth = variableWdth, hght = variableHght)
+                # fontVariations = dict(wdth = variableWdth , hght = variableHght)
                 )
+            # print(lineTxt.fontVariations())
+            # TODO
+            # text_width, text_height = textSize(lineTxt)
+            # print(text_width, text_height)
+            # print(char, displayTextList[line][char])
         text(lineTxt, (xOffset, yOffset))
-        yOffset -= FONTSIZE
+        # yOffset -= FONTSIZE # TODO
 
 
 # Save the animation as a gif
