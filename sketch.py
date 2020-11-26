b3313472751ed244d8a740c51d5ff713020740df
rony ginosar <rony.ginosar@mail.huji.ac.il>
2020-11-26 17:31:04 +0200

    grid fully working

--- sketch.py
+++ sketch.py
@@ -8,6 +8,17 @@ from drawBot import * # needed for using as python module
 # NTH - make input text
 # NTH - interactive with mouse
 
+# DISTRIBUTION LOGIC
+# rand #cols num
+# sum them - s1
+# width - w
+# scale all by w/s1
+# sum of all will be w
+# why?
+# the probability is the diff
+# in the original logic the numbers are monotonic decreasing, the expectance is also decreasing
+# in this idea, the expected is k/2
+
 
 # CONSTANTS:
 # DISPLAYTEXT = "מטבחים"
@@ -35,37 +46,29 @@ LINEHEIGHT = FONTSIZE*0.7
 DISPLAYWIDTH, DISPLAYHEIGHT = 500, LINEHEIGHT*LINES
 SCREENWIDTH = DISPLAYWIDTH + MARGINS*2
 SCREENHEIGHT = DISPLAYHEIGHT + MARGINS*2
-minLetterWidth = int(DISPLAYWIDTH * 0.1)
-maxLetterWidth = int(DISPLAYWIDTH * 0.4)
-minLetterHeight = int(DISPLAYHEIGHT* 0.1)
-maxLetterHeight = int(DISPLAYHEIGHT*0.8)
+# minLetterWidth = int(DISPLAYWIDTH * 0.1)
+# maxLetterWidth = int(DISPLAYWIDTH * 0.4)
+# minLetterHeight = int(DISPLAYHEIGHT* 0.1)
+# maxLetterHeight = int(DISPLAYHEIGHT*0.8)
 
 
 
 FRAME_DURATION = TOTALDURATION / TOTALFRAMES
 
 # INITIALIZE VARIABLES
-xOffset = SCREENWIDTH - MARGINS # since we align right for hebrew
+# xOffset = SCREENWIDTH - MARGINS # since we align right for hebrew
 yOffset = MARGINS
-accumulatedHeight = [yOffset] * CHARS_IN_LINE # acc height per column
-widthcontainer = [0] * CHARS_IN_LINE # tracking the 1st row widths to use
+# accumulatedHeight = [yOffset] * CHARS_IN_LINE # acc height per column
+# widthcontainer = [0] * CHARS_IN_LINE # tracking the 1st row widths to use
 
 # HELPER FUNCTIONS
 
 colunmwidths = [0]*CHARS_IN_LINE
-# rowheights = []*LINES
+accumulatedHeight = [0]*CHARS_IN_LINE ## TODO in rowheights
+rowheights = []
 shade = 0
 
-def rowDistribution():
-# rand 6 num
-# sum them - s1
-# width - w
-# scale all by w/s1
-# sum of all will be w
-# why?
-# the probability is the diff
-# in the original logic the numbers are monotonic decreasing, the expectance is also decreasing
-# in this idea, the expected is k/2
+def colDistribution():
     s = 0
     for i in range(CHARS_IN_LINE):
         r = randint(FONT_MIN_WIDTH, FONT_MAX_WIDTH)
@@ -81,11 +84,35 @@ def rowDistribution():
     # fix last
     remainder = w - sum(colunmwidths)
     colunmwidths[0] += remainder
+
     return
+
+def rowDistribution():
+    s = 0
+    heights = [0]*LINES
+    for i in range(LINES):
+        r = randint(FONT_MIN_HEIGHT, FONT_MAX_HEIGHT)
+        s += r
+        heights[i] = r
+    h = DISPLAYHEIGHT
+    scale = h/s
+    for i in range(LINES):
+        heights[i] *= scale
+        heights[i] = int(heights[i])
+    # fix last
+    remainder = h - sum(heights)
+    heights[0] += remainder
+
+    rowheights.append(heights)
+    return
+
 
 # MAIN FUNCTION
 newDrawing() # needed for using as python module
 for frame in range(TOTALFRAMES):
+    yOffset = MARGINS
+    accumulatedHeight = [0]*CHARS_IN_LINE
+
     # Draw the background rectangle
     newPage(SCREENWIDTH, SCREENHEIGHT)
     fill(BACKGROUND_R/255, BACKGROUND_G/255, BACKGROUND_B/255)
@@ -96,20 +123,23 @@ for frame in range(TOTALFRAMES):
     # fill(0)
     # rect(0,0, minLetterWidth,  minLetterHeight)
     # rect(100,0, maxLetterWidth, maxLetterHeight)
+    stroke(0)
+    fill(None)
 
-    for l in range(LINES):
+    for c in range(CHARS_IN_LINE):
         rowDistribution()
-        xOffset = SCREENWIDTH - MARGINS
 
-        stroke(0)
-        fill(None)
-        # print(colunmwidths)
-        for i in range(CHARS_IN_LINE):
-            width = colunmwidths[i]
-            height = 100
+    colDistribution()
+    for l in range(LINES): # rows
+        xOffset = SCREENWIDTH - MARGINS
+        for c in range(CHARS_IN_LINE): # cols
+            height = rowheights[c][l]
+            width = colunmwidths[c]
             xOffset -= width
-            rect(xOffset, yOffset, width, height)
-        yOffset +=100
+            rect(xOffset, yOffset+accumulatedHeight[c], width, height)
+            accumulatedHeight[c] += height
+
+
 
 
     # for line in range(LINES):
